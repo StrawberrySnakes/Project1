0a6a89e837a9cb84d30f05a745a5f924cb6f3844
DJ <djs9826@rit.edu>
2026-03-01 23:33:38 -0500

    comments

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 const js = require("@eslint/js");
 const globals = require("globals");
-
+// taken from https://eslint.org/docs/latest/use/configure/configuration-files#configuration-based-on-glob-patterns
+//To deal with server and client side code in the same project => need to specify different globals.
 module.exports = [
   js.configs.recommended,
   {
